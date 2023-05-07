f4b4f8042b5567e398ef8507694ae2a485090718
Nodirbek Rasuljonov <nodirbekmaqsudjonovich@gmail.com>
2023-05-07 11:44:15 +0200

    app is going

---
+++ lib/core/constants/color_const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorConst{
+  static Color kWhiteColor=Colors.white;
+  static Color kTransparentColor=Colors.transparent;
+}
--- lib/pages/home_view/view/home_view.dart
+++ lib/pages/home_view/view/home_view.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:myapp/core/constants/color_const.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  HomeView({super.key});
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorConst.kWhiteColor,
+      key: _scaffoldKey,
+      drawer: Container(
+        height: 800,
+        color: Colors.amber,
+        width: 300.h,
+        alignment: Alignment.center,
+        child: Text("Salom"),
+      ),
       appBar: AppBar(
+        backgroundColor: ColorConst.kTransparentColor,
+        leading: Container(),
         elevation: 0.0,
-        
+        actions: [
+          GestureDetector(
+            onTap: () {
+              _scaffoldKey.currentState?.openDrawer();
+            },
+            child: Image.asset("assets/images/menu.png"),
+          )
+        ],
       ),
     );
   }
